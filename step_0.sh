10d046a16ed46687677d277a47692863145ab603
Felix Stegmaier <stegmaier.felix@gmail.com>
2019-06-15 16:34:09 +0200

    Add packages and fix stuff

--- step_0.sh
+++ step_0.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -ex
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get -y install ansible python2.7 python-apt git
+sudo apt-get -y install ansible python2.7 python-apt git apt-transport-https
 sudo reboot now
